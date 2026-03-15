b9c435406e9a97f1019368adfecdcc7900409bc6
Gabriel Medina <gabrieljosemedina55@gmail.com>
2026-03-15 18:30:57 -0300

    Update Controlador_Trafico_Nuevo.py with changes

--- TP1/Ejercicio_2_Entorno_DInámico/Controlador_Trafico_Nuevo.py
+++ TP1/Ejercicio_2_Entorno_DInámico/Controlador_Trafico_Nuevo.py
@@ -2,73 +2,105 @@ class ControladorTrafico:
 
     def __init__(self, agentes):
         self.agentes = agentes
+        self.agentes_replanificar = set()
+
+        # planificación inicial
+        for agente in self.agentes:
+            agente.mover()
 
 
     def siguiente_posicion(self, agente):
 
-        # Si no hay frontera no hay siguiente movimiento
         if not agente.frontera:
             return None
 
-        _, nodo = agente.frontera[0]   # mejor nodo del heap
+        _, nodo = agente.frontera[0]
         return (nodo.x, nodo.y)
 
 
-    def detectar_conflictos(self):
+    def prioridad(self, a, b):
 
-        conflictos = []
+        da = a.heuristica(a.x, a.y)
+        db = b.heuristica(b.x, b.y)
 
-        for i in range(len(self.agentes)):
-            for j in range(i + 1, len(self.agentes)):
-
-                a = self.agentes[i]
-                b = self.agentes[j]
-
-                sig_a = self.siguiente_posicion(a)
-                sig_b = self.siguiente_posicion(b)
-
-                if sig_a is None or sig_b is None:
-                    continue
-
-                # misma celda
-                if sig_a == sig_b:
-                    conflictos.append((a, b))
-
-                # intercambio de posiciones
-                if sig_a == (b.x, b.y) and sig_b == (a.x, a.y):
-                    conflictos.append((a, b))
-
-        return conflictos
-
-
-    def resolver_conflicto(self, a, b):
-
-        # prioridad al que esté más cerca del objetivo
-        if a.heuristica(a.x, a.y) < b.heuristica(b.x, b.y):
-            ganador = a
-            perdedor = b
+        if da < db:
+            return a, b
         else:
-            ganador = b
-            perdedor = a
+            return b, a
 
-        # el perdedor retrocede si puede
-        if perdedor.visitados:
-            perdedor.x, perdedor.y = perdedor.visitados.pop()
 
-        # el ganador avanza
-        ganador.mover()
+    def detectar_conflicto(self, a, b):
+
+        sig_a = self.siguiente_posicion(a)
+        sig_b = self.siguiente_posicion(b)
+
+        if sig_a is None or sig_b is None:
+            return None
+
+        # misma celda
+        if sig_a == sig_b:
+            return "misma"
+
+        # intercambio
+        if sig_a == (b.x, b.y) and sig_b == (a.x, a.y):
+            return "intercambio"
+
+        return None
+
+
+    def recalcular_A(self, agente):
+
+        agente.frontera = []
+        agente.visitados = []
+        agente.mover()
 
 
     def actualizar(self):
 
-        conflictos = self.detectar_conflictos()
+        conflictos = []
+
+        for i in range(len(self.agentes)):
+            for j in range(i+1, len(self.agentes)):
+
+                a = self.agentes[i]
+                b = self.agentes[j]
+
+                tipo = self.detectar_conflicto(a,b)
+
+                if tipo:
+                    conflictos.append((a,b,tipo))
+
 
         if conflictos:
 
-            for a, b in conflictos:
-                self.resolver_conflicto(a, b)
+            for a,b,tipo in conflictos:
+
+                ganador, perdedor = self.prioridad(a,b)
+
+                if tipo == "misma":
+
+                    ganador.mover()
+                    # perdedor se queda quieto
+                    self.agentes_replanificar.add(perdedor)
+
+
+                elif tipo == "intercambio":
+
+                    ganador.mover()
+
+                    if perdedor.visitados:
+                        perdedor.x, perdedor.y = perdedor.visitados.pop()
+
+                    self.agentes_replanificar.add(perdedor)
+
 
         else:
 
             for agente in self.agentes:
+
+                if agente in self.agentes_replanificar:
+                    self.recalcular_A(agente)
+
                 agente.mover()
+
+            self.agentes_replanificar.clear()

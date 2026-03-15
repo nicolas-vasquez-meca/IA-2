ad6f4ff32e067aea580945b2392f9df11e97ae53
Nico <nicovasquezmoretti@gmail.com>
2026-03-15 20:20:57 -0300

    trabajando en resolver conflicto()

--- TP1/Ejercicio_2_Entorno_DInámico/Controlador_Trafico_Nuevo.py
+++ TP1/Ejercicio_2_Entorno_DInámico/Controlador_Trafico_Nuevo.py
@@ -1,106 +1,63 @@
+from TP1.Agentes.Agente import Agente
+
 class ControladorTrafico:
 
     def __init__(self, agentes):
-        self.agentes = agentes
+        self.agentes: list = agentes
         self.agentes_replanificar = set()
 
+        self.paso = 0
         # planificación inicial
         for agente in self.agentes:
-            agente.mover()
+
+            while not agente.mover():
+                pass
 
 
-    def siguiente_posicion(self, agente):
-
-        if not agente.frontera:
-            return None
-
-        _, nodo = agente.frontera[0]
-        return (nodo.x, nodo.y)
-
-
-    def prioridad(self, a, b):
+    def prioridad(self, a: Agente, b: Agente):
 
         da = a.heuristica(a.x, a.y)
         db = b.heuristica(b.x, b.y)
 
         if da < db:
-            return a, b
+            return a
         else:
-            return b, a
+            return b
+        
+        
+    def Step(self):
+        self.paso = self.paso + 1
+
+        estado_actual: list = []
+
+        for agente in self.agentes:
+            estado_actual.append(agente.camino[self.paso])
+
+        self.resolver_conflicto(estado_actual)
 
 
-    def detectar_conflicto(self, a, b):
+    def resolver_conflicto(self, estado_actual: list):
 
-        sig_a = self.siguiente_posicion(a)
-        sig_b = self.siguiente_posicion(b)
+        # diccionario: clave = (x,y) , valor = lista de indices donde aparece
+        ocurrencias = {}
 
-        if sig_a is None or sig_b is None:
-            return None
+        for i, posicion in enumerate(estado_actual): # 0, (1,2)
 
-        # misma celda
-        if sig_a == sig_b:
-            return "misma"
+            if posicion not in ocurrencias:
+                ocurrencias[posicion] = []            #ocurrencias[1,2] = [] #ocurrencias[4,5] = []
 
-        # intercambio
-        if sig_a == (b.x, b.y) and sig_b == (a.x, a.y):
-            return "intercambio"
+            ocurrencias[posicion].append(i)           # ocurrencias[1,2] = (0,3,4)  # ocurrencias[4,5] = (1,2,5)
 
-        return None
+        # buscar los que se repiten
+        conflictos = {}
+        total_repeticiones = 0
 
+        for posicion, indices in ocurrencias.items(): # (1,2) => (0, 3, 4) len => 3
 
-    def recalcular_A(self, agente):
+            if len(indices) > 1:
+                conflictos[posicion] = indices       # (1,2) 
+                total_repeticiones += len(indices)
+        
+        
 
-        agente.frontera = []
-        agente.visitados = []
-        agente.mover()
-
-
-    def actualizar(self):
-
-        conflictos = []
-
-        for i in range(len(self.agentes)):
-            for j in range(i+1, len(self.agentes)):
-
-                a = self.agentes[i]
-                b = self.agentes[j]
-
-                tipo = self.detectar_conflicto(a,b)
-
-                if tipo:
-                    conflictos.append((a,b,tipo))
-
-
-        if conflictos:
-
-            for a,b,tipo in conflictos:
-
-                ganador, perdedor = self.prioridad(a,b)
-
-                if tipo == "misma":
-
-                    ganador.mover()
-                    # perdedor se queda quieto
-                    self.agentes_replanificar.add(perdedor)
-
-
-                elif tipo == "intercambio":
-
-                    ganador.mover()
-
-                    if perdedor.visitados:
-                        perdedor.x, perdedor.y = perdedor.visitados.pop()
-
-                    self.agentes_replanificar.add(perdedor)
-
-
-        else:
-
-            for agente in self.agentes:
-
-                if agente in self.agentes_replanificar:
-                    self.recalcular_A(agente)
-
-                agente.mover()
-
-            self.agentes_replanificar.clear()
+        

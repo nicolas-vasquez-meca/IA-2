12d935c6c79c3b65b055ca15b83905bd38445228
Nico <nicovasquezmoretti@gmail.com>
2026-04-17 16:48:24 -0300

    ejemplo de uso

--- TP2/fuzzy_ctrl.py
+++ TP2/fuzzy_ctrl.py
@@ -1,5 +1,6 @@
 import numpy as np
 import skfuzzy as fuzy
+from skfuzzy import control as ctrl
 
 class fuzzy_ctrl:
     def __init__(self, V_obj, Ve):
@@ -15,31 +16,34 @@ class fuzzy_ctrl:
     def apertura(self):
         
         # Variables difusas
-        temperatura = fuzy.Antecedent(np.arange(0, 41, 1), 'temperatura')
-        humedad = fuzy.Antecedent(np.arange(-10,11,1), 'humedad')
+        temperatura = ctrl.Antecedent(np.arange(-10, 41, 1), 'temperatura')
+        humedad = ctrl.Antecedent(np.arange(35,81,1), 'humedad')
         
-        Apertura = fuzy.Consequent(np.arange(0, 101, 1), 'velocidad')
+        climatizacion = ctrl.Consequent(np.arange(0, 101, 1), 'climatizacion')
 
         # Funciones de pertenencia
-        temperatura['pp'] = fuzy.trimf(temperatura.universe, [0, 10, 20])
+        temperatura['PP'] = fuzy.trimf(temperatura.universe, [0, 10, 20])
+        temperatura['Z'] = fuzy.trimf(temperatura.universe, [-10, 0, 10])
 
-        Apertura['lenta'] = fuzy.trimf(Apertura.universe, [0, 0, 50])
-        Apertura['media'] = fuzy.trimf(Apertura.universe, [25, 50, 75])
-        Apertura['rapida'] = fuzy.trimf(Apertura.universe, [50, 100, 100])
+        humedad['M'] = fuzy.trimf(humedad.universe, [35, 50, 65])
+        humedad['A'] = fuzy.trimf(humedad.universe, [50, 65, 80])
+
+        climatizacion['PP'] = fuzy.trimf(climatizacion.universe, [0, 10, 20])
+        climatizacion['Z'] = fuzy.trimf(climatizacion.universe, [-10, 0, 10])
 
         # Reglas
-        regla1 = fuzy.Rule(temperatura['baja'], Apertura['lenta'])
-        regla2 = fuzy.Rule(temperatura['media'], Apertura['media'])
-        regla3 = fuzy.Rule(temperatura['alta'], Apertura['rapida'])
+        regla1 = ctrl.Rule(temperatura['PP'] & humedad['A'], climatizacion["PP"])
+        regla2 = ctrl.Rule(temperatura['Z'] & humedad['M'], climatizacion["Z"])
 
         # Sistema
-        sistema_ctrl = fuzy.ControlSystem([regla1, regla2, regla3])
-        sistema = fuzy.ControlSystemSimulation(sistema_ctrl)
+        sistema_ctrl = ctrl.ControlSystem([regla1, regla2])
+        sistema = ctrl.ControlSystemSimulation(sistema_ctrl)
 
         # Entrada
-        sistema.input['temperatura'] = self.Ve
+        sistema.input['temperatura'] = 7
+        sistema.input['humedad'] = 60
 
         # Inferencia
         sistema.compute()
 
-        return sistema.output['Salida']
+        return sistema.output['climatizacion']
--- TP2/main.py
+++ TP2/main.py
@@ -1,5 +1,5 @@
-import fuzzy_ctrl
+from fuzzy_ctrl import fuzzy_ctrl
 
-if __name__ == "main":
+if __name__ == "__main__":
     ctrl = fuzzy_ctrl(25, 35)
     print(ctrl.apertura())

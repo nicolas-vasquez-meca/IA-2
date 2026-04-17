cad793f07b21180626e6bf0e9c3d61b7fa5f8af6
Nico <nicovasquezmoretti@gmail.com>
2026-04-17 15:28:59 -0300

    trabajando en ctrl

---
+++ TP2/fuzzy_ctrl.py
@@ -0,0 +1,49 @@
+
+import numpy as np
+import skfuzzy as fuzy
+from skfuzzy import control as ctrl
+
+class fuzzy_ctrl:
+    def __init__(self, V_obj, Ve, C, R,):
+        self.V_obj = V_obj
+        self.Ve = Ve
+        self.C = C
+        self.R = R
+
+    def set_Obj(self, V_obj):
+        self.V_obj = V_obj
+    
+    def set_Ve(self, Ve):
+        self.V_obj = Ve
+    
+    def apertura(self):
+        
+        # Variables difusas
+        temperatura = ctrl.Antecedent(np.arange(0, 41, 1), 'temperatura')
+        Apertura = ctrl.Consequent(np.arange(0, 101, 1), 'velocidad')
+
+        # Funciones de pertenencia
+        temperatura['baja'] = fuzy.trimf(temperatura.universe, [0, 0, 20])
+        temperatura['media'] = fuzy.trimf(temperatura.universe, [10, 20, 30])
+        temperatura['alta'] = fuzy.trimf(temperatura.universe, [20, 40, 40])
+
+        Apertura['lenta'] = fuzy.trimf(Apertura.universe, [0, 0, 50])
+        Apertura['media'] = fuzy.trimf(Apertura.universe, [25, 50, 75])
+        Apertura['rapida'] = fuzy.trimf(Apertura.universe, [50, 100, 100])
+
+        # Reglas
+        regla1 = ctrl.Rule(temperatura['baja'], Apertura['lenta'])
+        regla2 = ctrl.Rule(temperatura['media'], Apertura['media'])
+        regla3 = ctrl.Rule(temperatura['alta'], Apertura['rapida'])
+
+        # Sistema
+        sistema_ctrl = ctrl.ControlSystem([regla1, regla2, regla3])
+        sistema = ctrl.ControlSystemSimulation(sistema_ctrl)
+
+        # Entrada
+        sistema.input['temperatura'] = 28
+
+        # Inferencia
+        sistema.compute()
+
+        print("Velocidad de salida:", sistema.output['velocidad'])

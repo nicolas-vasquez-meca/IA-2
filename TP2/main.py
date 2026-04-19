7ad476fed8d8386e82f6be5b44ae33735e7574fc
Gabriel Medina <gabrieljosemedina55@gmail.com>
2026-04-19 20:43:05 -0300

    TP2: update main to use FuzzyCtrl controller

--- TP2/main.py
+++ TP2/main.py
@@ -1,3 +1,34 @@
+from Planta import simular, Rv_max
+import matplotlib.pyplot as plt
+from TP2.main_ejemplo import FuzzyCtrl
+
+# -----------------------------
+# Controlador (ejemplo) (aqui ya le toca a los de controlador)
+# -----------------------------
+ctrl_difuso = FuzzyCtrl(Rv_max)
+
+# -----------------------------
+# MAIN
+# -----------------------------
+def main():
+
+    # Ejecutar simulación
+    t, v, ve = simular(ctrl_difuso)
+
+    # Graficar
+    plt.figure()
+    plt.plot(t, ve, label="Exterior", linestyle="dashed")
+    plt.plot(t, v, label="Interior (controlado)")
+    plt.xlabel("Tiempo (horas)")
+    plt.ylabel("Temperatura (°C)")
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+"""""
 from fuzzy_ctrl import fuzzy_ctrl
 from Planta import temperatura_exterior
 from Planta import planta
@@ -15,4 +46,5 @@ if __name__ == "__main__":
 
         ctrl.set_Ve(       )
         ctrl.set_V(       )
-        apertura = ctrl.control()
+        apertura = ctrl.control()/
+"""""
